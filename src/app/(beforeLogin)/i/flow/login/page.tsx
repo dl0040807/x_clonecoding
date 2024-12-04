3636cc06f4188aaceb3d5353e253d5088684a335
Yehwan Lee <dl0040807@gmail.com>
2024-12-04 14:47:41 +0900

    first commit

---
+++ src/app/(beforeLogin)/i/flow/login/page.tsx
@@ -0,0 +1,5 @@
+// import styles from "@/app/page.module.css";
+
+export default function Login() {
+  return <main>로그인 모달</main>;
+}
---
+++ src/app/(beforeLogin)/i/flow/signup/page.tsx
@@ -0,0 +1,5 @@
+// import styles from "@/app/page.module.css";
+
+export default function SignUp() {
+  return <main>회원가입 모달</main>;
+}
